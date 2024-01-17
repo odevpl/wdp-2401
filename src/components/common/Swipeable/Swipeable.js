30afeaa12e8f38fbde1d422c6a310d1660b0c0be
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-17 16:20:23 +0100

    Add code of Swipeable

--- src/components/common/Swipeable/Swipeable.js
+++ src/components/common/Swipeable/Swipeable.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import Hammer from 'hammerjs';
 import PropTypes from 'prop-types';
 
 const Swipeable = ({ leftAction, rightAction, children }) => {
-  return <div>{children}</div>;
+  const swipeContainerRef = useRef(null);
+
+  useEffect(() => {
+    const hammer = new Hammer(swipeContainerRef.current);
+
+    hammer.on('swipeleft', () => {
+      if (leftAction) {
+        leftAction();
+      }
+    });
+
+    hammer.on('swiperight', () => {
+      if (rightAction) {
+        rightAction();
+      }
+    });
+
+    return () => {
+      hammer.destroy();
+    };
+  }, [leftAction, rightAction]);
+
+  return (
+    <div ref={swipeContainerRef} style={{ overflow: 'hidden', touchAction: 'pan-y' }}>
+      {children}
+    </div>
+  );
 };
 
 Swipeable.propTypes = {
