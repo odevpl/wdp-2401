f0614773407eb0d04d4144e586a116142b2a09b5
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-23 23:43:53 +0100

    Add func handleClickClose in StickyBar

--- src/components/common/StickyBar/StickyBar.js
+++ src/components/common/StickyBar/StickyBar.js
@@ -4,13 +4,22 @@ import styles from './StickyBar.module.scss';
 import Button from '../Button/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch } from 'react-redux';
+import { toggleProductCompared } from '../../../redux/productsRedux';
 
 const StickyBar = ({ comparedProducts }) => {
+  const dispatch = useDispatch();
+
+  const handleClickClose = itemId => e => {
+    e.preventDefault();
+    dispatch(toggleProductCompared(itemId));
+  };
+
   return (
     <div className={styles.stickyBar}>
       {comparedProducts.map(item => (
         <div key={item.id} className={styles.box}>
-          <Button className={styles.close}>
+          <Button className={styles.close} onClick={handleClickClose(item.id)}>
             <FontAwesomeIcon icon={faTimes} title='Close' />
           </Button>
           <img
