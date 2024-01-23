d5b44a271c66ca7c6127f922a2f3c7b81273c10b
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-23 23:20:18 +0100

    Add button close to StickyBar and style it

--- src/components/common/StickyBar/StickyBar.js
+++ src/components/common/StickyBar/StickyBar.js
@@ -2,16 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './StickyBar.module.scss';
 import Button from '../Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const StickyBar = ({ comparedProducts }) => {
   return (
     <div className={styles.stickyBar}>
       {comparedProducts.map(item => (
-        <img
-          key={item.id}
-          alt={item.name}
-          src={process.env.PUBLIC_URL + `/images/furnitures/${item.image}.webp`}
-        />
+        <div key={item.id} className={styles.box}>
+          <Button className={styles.close}>
+            <FontAwesomeIcon icon={faTimes} title='Close' />
+          </Button>
+          <img
+            alt={item.name}
+            src={process.env.PUBLIC_URL + `/images/furnitures/${item.image}.webp`}
+          />
+        </div>
       ))}
       <Button variant='small'>Compare</Button>
     </div>
