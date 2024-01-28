a3679b138dd5955a63dd9d1fe5e63e98c4976191
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-28 21:10:36 +0100

    Add hover handlers for component and stars in ProductStars

--- src/components/common/ProductStars/ProductStars.js
+++ src/components/common/ProductStars/ProductStars.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './ProductStars.module.scss';
@@ -11,16 +11,67 @@ import { addProductRating } from '../../../redux/productsRedux';
 const ProductStars = ({ id, stars, myRating }) => {
   const dispatch = useDispatch();
 
-  const handleClickStar = (idProduct, starKey) => e => {
+  const [componentOnHover, setComponentOnHover] = useState(false);
+  const [hoveredStars, setHoveredStars] = useState(null);
+
+  const handleComponentHover = () => {
+    setComponentOnHover(true);
+  };
+
+  const handleComponentLeave = () => {
+    setComponentOnHover(false);
+  };
+
+  const handleStarsHover = starKey => {
+    setHoveredStars(starKey);
+  };
+
+  const handleStarsLeave = () => {
+    setHoveredStars(null);
+  };
+
+  const handleStarClick = (idProduct, starKey) => e => {
     e.preventDefault();
     dispatch(addProductRating({ idProduct, starKey }));
   };
 
+  if (componentOnHover) {
+    return (
+      <div
+        className={styles.stars}
+        onMouseEnter={handleComponentHover}
+        onMouseLeave={handleComponentLeave}
+      >
+        {[1, 2, 3, 4, 5].map(i => (
+          <a
+            key={i}
+            href='#'
+            onClick={handleStarClick(id, i)}
+            onMouseEnter={() => handleStarsHover(i)}
+            onMouseLeave={handleStarsLeave}
+          >
+            {i <= hoveredStars ? (
+              <FontAwesomeIcon icon={faStar} className={styles.myRating}>
+                {i} stars
+              </FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+            )}
+          </a>
+        ))}
+      </div>
+    );
+  }
+
   if (myRating !== 0) {
     return (
-      <div className={styles.stars}>
+      <div
+        className={styles.stars}
+        onMouseEnter={handleComponentHover}
+        onMouseLeave={handleComponentLeave}
+      >
         {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#' onClick={handleClickStar(id, i)}>
+          <a key={i} href='#' onClick={handleStarClick(id, i)}>
             {i <= myRating ? (
               <FontAwesomeIcon icon={faStar} className={styles.myRating}>
                 {i} stars
@@ -36,9 +87,13 @@ const ProductStars = ({ id, stars, myRating }) => {
 
   if (myRating === 0) {
     return (
-      <div className={styles.stars}>
+      <div
+        className={styles.stars}
+        onMouseEnter={handleComponentHover}
+        onMouseLeave={handleComponentLeave}
+      >
         {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#' onClick={handleClickStar(id, i)}>
+          <a key={i} href='#' onClick={handleStarClick(id, i)}>
             {i <= stars ? (
               <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
             ) : (
