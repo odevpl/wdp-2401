8ceacbf72ce4efda5b266884039c7ff6be789936
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-28 15:56:31 +0100

    Create new component ProductStars and styles for it

---
+++ src/components/common/ProductStars/ProductStars.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './ProductStars.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+
+const ProductStars = ({ id, name, stars }) => {
+  return (
+    <div className={styles.content}>
+      <h5>{name}</h5>
+      <div className={styles.stars}>
+        {[1, 2, 3, 4, 5].map(i => (
+          <a key={i} href='#'>
+            {i <= stars ? (
+              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+            ) : (
+              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+            )}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+ProductStars.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  stars: PropTypes.number,
+};
+
+export default ProductStars;
