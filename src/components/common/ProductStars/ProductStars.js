f713688f7ece03cc9714fb296b95670f0f331774
Julia Glinka <julia.w.glinka@gmail.com>
2024-01-28 15:59:50 +0100

    Fix component ProductStars

--- src/components/common/ProductStars/ProductStars.js
+++ src/components/common/ProductStars/ProductStars.js
@@ -6,28 +6,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 
-const ProductStars = ({ id, name, stars }) => {
+const ProductStars = ({ id, stars }) => {
   return (
-    <div className={styles.content}>
-      <h5>{name}</h5>
-      <div className={styles.stars}>
-        {[1, 2, 3, 4, 5].map(i => (
-          <a key={i} href='#'>
-            {i <= stars ? (
-              <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
-            ) : (
-              <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
-            )}
-          </a>
-        ))}
-      </div>
+    <div className={styles.stars}>
+      {[1, 2, 3, 4, 5].map(i => (
+        <a key={i} href='#'>
+          {i <= stars ? (
+            <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+          ) : (
+            <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+          )}
+        </a>
+      ))}
     </div>
   );
 };
 
 ProductStars.propTypes = {
   id: PropTypes.string,
-  name: PropTypes.string,
   stars: PropTypes.number,
 };
 
